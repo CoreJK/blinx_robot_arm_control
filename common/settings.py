4ea80e914081f2bf00c15f4519eeb2d5cfc806f3
任伟明 <corejk@163.com>
2024-07-25 16:56:15 +0800

    📘 docs: 更新上位机 READM.md 等文档
     删除不再使用的资源文件和界面文件

--- common/settings.py
+++ common/settings.py
@@ -10,8 +10,5 @@ LOG_FILE_PATH = PROJECT_ROOT_PATH / "logs/record_{time}.log"
 IP_PORT_INFO_FILE_PATH = PROJECT_ROOT_PATH / "config/Socket_Info"
 WIFI_INFO_FILE_PATH = PROJECT_ROOT_PATH / "config/WiFi_Info"
 
-# 机械臂模型配置文件路径
-ROBOT_MODEL_CONFIG_FILE_PATH = PROJECT_ROOT_PATH / "config/robot_mdh_parameters.yaml"
-
 # 图标路径
 WINDOWS_ICON_PATH = PROJECT_ROOT_PATH / "assets/icons/Robot_arm_log.png"
